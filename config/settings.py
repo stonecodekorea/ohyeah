630cb7ae57b8a6002ca81ba95d7f46373cf86dae
stonecodekorea <kwangseokj@naver.com>
2022-08-07 03:52:29 +0900

    modify book out & book clean status

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'common.apps.CommonConfig',
     'ohyeah.apps.OhyeahConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
